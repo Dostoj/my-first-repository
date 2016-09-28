160acd2b1ec1dfb6e6572d350557f1b8df3a7457
Thomas Van Calster <thomas_vancalster@outlook.com>
2016-09-28 19:45:48 +0000

    exercises basics

---
+++ intro/basics.rb
@@ -0,0 +1,54 @@
+# exercise 1
+
+firstname = "Thomas"
+lastname = "Van Calster"
+
+puts firstname + " " + lastname
+
+puts "#{firstname} #{lastname}"
+
+# exercise 2
+
+puts 8523 / 1000
+puts 8523 % 1000 / 100
+puts 8523 % 100 / 10
+puts 8523 % 10
+
+# exercise 3
+
+movies = {
+  movie1: 1975,
+  movie2: 2004,
+  movie3: 2013,
+  movie4: 2001,
+  movie5: 1981 
+}
+    
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
+
+# exercise 4
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+# exercise 5
+
+puts 5 * 4 * 3 * 2* 1
+puts 6 * 5 * 4 * 3 * 2* 1
+puts 7 * 6 * 5 * 4 * 3 * 2* 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2* 1
+
+# exercise 6
+
+puts 4.30 * 4.30
+puts 52.23 * 52.23
+puts 101.88 * 101.88
