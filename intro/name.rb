ee0e965fd5ad1dc36e09d71783af460be35b2626
Thomas Van Calster <thomas_vancalster@outlook.com>
2016-10-01 19:33:26 +0000

    Add exercises variables

---
+++ intro/name.rb
@@ -0,0 +1,17 @@
+# exercise 1 + 4
+
+puts "what is your name?"
+name = gets.chomp
+
+puts "What is you last name?"
+lastname = gets.chomp
+
+
+puts "Hello " + name + " " + lastname
+
+# exercise 3
+
+10.times do puts name + " " + lastname
+    
+end
+
