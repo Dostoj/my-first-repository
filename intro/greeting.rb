b194305b989815e9b9eb75ca034e5cd79b908e28
Thomas Van Calster <thomas_vancalster@outlook.com>
2016-10-03 19:08:39 +0000

    exercises methods

---
+++ intro/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+    greeting = "Hello there, " + name + ". How are you today?"
+end
+
+puts greeting("Thomas")
---
+++ intro/mutate.rb
@@ -0,0 +1,20 @@
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+
+puts "Before mutate method: #{a}"
+puts mutate(a)
+puts "After mutate method: #{a}"
+
+
+a = [1, 2, 3]
+
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
